71ec5ffa4235a9eeb03adfcd8909613bd8e6aa76
jessekroon <jessekroonmail@gmail.com>
2020-04-11 22:21:09 +0200

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends React.Component {
                 <div className="inner">
                     <Link to="/" className="image avatar"><img src={avatar} alt="" /></Link>
                     <h1>This Rocket is Launching Soon<br /> ... <strong>Readyyy, Steadyyy... <a href="https://nl.wikipedia.org/wiki/Trompet" target="_blank">!!!</a></strong>,<br /> 
-                    crafted by <a href="https://www.intertoys.nl/shop/nl/intertoys/lego-duplo-stoomtrein-10874-000" target="_blank"><strong>Jazzy</strong></a>.</h1>
+                    crafted by <a href="https://www.intertoys.nl/shop/nl/intertoys/lego-duplo-stoomtrein-10874-000" target="_blank"><strong>Freddie</strong></a>.</h1>
                 </div>
                 <Footer />
             </header>
