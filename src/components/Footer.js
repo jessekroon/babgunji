3549d032aa73cc4f860efca9cefcd6c122c904a5
jessekroon <jessekroonmail@gmail.com>
2020-04-12 20:06:02 +0200

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://twitter.com/cryptolin" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://theflyingphilosopher.com/" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                         <li><a href="https://github.com/conradlin" target="_blank" className="icon fa-github"><span className="label">Github</span></a></li>
                         <li><a href="https://conradlin.com/subscribe/" target="_blank" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
