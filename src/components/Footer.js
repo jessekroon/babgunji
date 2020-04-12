15a9bf5a34b073d23b1cceb2832acfea58af89db
jessekroon <jessekroonmail@gmail.com>
2020-04-12 21:40:16 +0200

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,11 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="https://theflyingphilosopher.com/" target="_blank" className="icon fa-square"><span className="label">Twitter</span></a></li>
                         <li><a href="https://github.com/conradlin" target="_blank" className="icon fa-square"><span className="label">Github</span></a></li>
-                        <li><a href="https://conradlin.com/subscribe/" target="_blank" className="icon fa-square"><span className="label">Email</span></a></li>
+                        <li><a href="https://conradlin.com/subscribe/" target="_blank" className="icon fa-rocket"><span className="label">Email</span></a></li>
+                        <li><a href="extra icon" target="_blank" className="icon fa-square"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Kermit without a permit</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>&copy; Kermit without a permit</li><li>Design: <a href="http://html5up.net">Fruhstuck!</a></li>
                     </ul>
                 </div>
             </div>
